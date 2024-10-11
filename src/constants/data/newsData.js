650a35be8914a82534ea6faed751abfb382ff009
RedwanNewaz <redwan06me@gmail.com>
2024-10-11 16:19:16 -0500

    updates

--- src/constants/data/newsData.js
+++ src/constants/data/newsData.js
@@ -28,12 +28,6 @@ export const newsData = [
         title: "Ubiquitous Robots 2024 Papers",
         content: "Two papers got accepted to  International Conference on Ubiquitous Robots, 2024, Adaptive Dynamic Window Approach for Robot Navigation in Disturbance Vector Fields, and Randomized Multi-Robot Patrolling with Unidirectional Visibility.",
         date: "FEB 2024",
-    },
-    {
-        id: 6,
-        title: "Ubiquitous Robots 2024 Papers",
-        content: "Two papers got accepted to  International Conference on Ubiquitous Robots, 2024, Adaptive Dynamic Window Approach for Robot Navigation in Disturbance Vector Fields, and Randomized Multi-Robot Patrolling with Unidirectional Visibility.",
-        date: "FEB 2024",
-    }  
+    }
   
 ]
